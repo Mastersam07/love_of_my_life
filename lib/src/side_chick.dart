e37a93c7419f85961f1cfd2f8ef2525150abc15f
Mastersam07 <abadasamuelosp@gmail.com>
2023-01-13 19:54:09 +0100

    :sparkles: chore: side chick

---
+++ lib/src/side_chick.dart
@@ -0,0 +1,23 @@
+import 'relationship.dart';
+
+abstract class SideChick extends Relationship {
+  @override
+  void methodA() {
+    throw UnimplementedError();
+  }
+
+  @override
+  void methodB() {
+    throw UnimplementedError();
+  }
+
+  @override
+  void methodC() {
+    throw UnimplementedError();
+  }
+
+  @override
+  void methodD() {
+    throw UnimplementedError();
+  }
+}
